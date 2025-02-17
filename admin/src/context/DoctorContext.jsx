ef1b50d771b57ab2e9446483bdb2ff410cb354e9
Happy-M1 <dumi4368@gmail.com>
2025-02-17 08:24:50 +0200

    Update backend url in DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://gogodabulamatshe-umelaphi-backend.onrender.com"; //import.meta.env.VITE_BACKEND_URL;
 
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
